601a1ee7cbadb6e0ef17eba63965c70c83e83d33
AidenBaiZe <15010085313qaz@gmail.com>
2025-09-21 17:23:38 +0800

    实现博客可点击文字系统 - 优化blog和todolist的交互功能

--- Assets/Scripts/APPs/Blog/BlogContentData.cs
+++ Assets/Scripts/APPs/Blog/BlogContentData.cs
@@ -19,4 +19,14 @@ public class BlogContentData : ScriptableObject
     public Color contentColor = Color.gray;
     public Font titleFont;
     public Font contentFont;
+    
+    [Header("可点击文字设置")]
+    public bool hasClickableText = false;           // 是否生成可点击文字
+    public string clickableText = "";               // 可点击文字内容
+    public Vector2 textSpawnPosition = Vector2.zero; // 文字生成位置
+    public float textFontSize = 16f;                // 文字字体大小
+    public Color textColor = Color.black;           // 文字颜色
+    public Vector2 textSize = new Vector2(200, 30); // 文字框大小
+    public string missionName = "";                 // 任务名称
+    public int missionIndex = 0;                    // 任务索引
 }
