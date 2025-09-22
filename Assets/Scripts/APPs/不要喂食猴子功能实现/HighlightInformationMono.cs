f551071e9cf8f482ea51b1307d722565fbd5c9de
AidenBaiZe <15010085313qaz@gmail.com>
2025-09-22 18:04:02 +0800

    debug

--- Assets/Scripts/APPs/不要喂食猴子功能实现/HighlightInformationMono.cs
+++ Assets/Scripts/APPs/不要喂食猴子功能实现/HighlightInformationMono.cs
@@ -9,6 +9,9 @@ using UnityEngine.EventSystems;
 
 public class HighlightInformationMono : MonoBehaviour
 {
+    // 添加静态HashSet记录已点击的文字
+    private static HashSet<string> clickedTexts = new HashSet<string>();
+    
     GameObject gameObjectList;
     public bool HighLightStringStarttoMove;
     public float MoveSpeed;
@@ -16,16 +19,25 @@ public class HighlightInformationMono : MonoBehaviour
     public string StringInformation;                     //任务相关信息
     public string missionName;                          //任务相关信息
     public int missionIndex;                            //任务相关信息
+
+    // 修改为string类型
+    public string textId;
+    
     void Start()
     {
         HighLightStringStarttoMove = false;
-        
+        if (clickedTexts.Contains(textId))
+        {
+            // 禁用
+            GetComponent<Collider2D>().enabled = false;
+            // 直接销毁
+            // Destroy(gameObject);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-
         if (HighLightStringStarttoMove)
         {
             // 获取目标位置的世界坐标
@@ -37,16 +49,13 @@ public class HighlightInformationMono : MonoBehaviour
                 tasktoDoListTextMono.AddTask(missionName, missionIndex);
             }
             
-
             Vector3 Targetposition = tasktoDoListTextMono.GetNewInformationPosition(0);                  //0待定
             
-
             transform.position= math.lerp(transform.position, Targetposition, MoveSpeed);
             //transform.position +=  MoveSpeed * Time.deltaTime *  (Vector3)math.normalize(new float3(Targetposition.x - transform.position.x, Targetposition.y - transform.position.y, Targetposition.z - transform.position.z));
 
             if (math.distance(Targetposition, transform.position) < 0.3)
             {
-                
                 HighLightStringStarttoMove = false;
                 
                 gameObjectList.GetComponent<TaskToDoListTextMono>(). AddInformation(0, StringInformation);                           //0是待修改的量
@@ -54,13 +63,19 @@ public class HighlightInformationMono : MonoBehaviour
             }
         }
     }
+    
     void OnMouseDown()
     {
+        // 检查点击
+        if (clickedTexts.Contains(textId))
+        {
+            return; 
+        }
+        clickedTexts.Add(textId);
+
         HighLightStringStarttoMove = true;
         Transform Texttransform= transform.Find("Text1");
         Texttransform.gameObject.GetComponent<TextMeshProUGUI>().text = StringInformation;
-
-
     }
     
 }
